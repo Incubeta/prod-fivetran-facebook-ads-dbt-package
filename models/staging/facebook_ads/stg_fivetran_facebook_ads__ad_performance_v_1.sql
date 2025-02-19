067a8f9e696cf601e8da7f12b8bde84376e83214
Miguelo Sana <miguelo@incubeta.com>
2025-02-19 16:22:04 +0100

    formatting

--- models/staging/facebook_ads/stg_fivetran_facebook_ads__ad_performance_v_1.sql
+++ models/staging/facebook_ads/stg_fivetran_facebook_ads__ad_performance_v_1.sql
@@ -1,10 +1,11 @@
 with base as (
-    select * 
+    select *
     from {{ ref('stg_fivetran_facebook_ads__ad_performance_v_1_tmp') }}
 ),
+
 fields as (
-    select 
-    _fivetran_synced,
+    select
+        _fivetran_synced,
     {{
         fivetran_utils.fill_staging_columns(
             source_columns=adapter.get_columns_in_relation(ref('stg_fivetran_facebook_ads__ad_performance_v_1_tmp')),
@@ -16,34 +17,35 @@ fields as (
             fivetran_utils.source_relation()
             }}
 
-            from base
-        ) 
-SELECT
+    from base
+)
+
+select
     _fivetran_synced,
-source_relation,
-    _fivetran_id AS  fivetran_id,
-    date AS  day,
-    ad_id AS  ad_id,
-    ad_name AS  ad_name,
-    adset_id AS  adset_id,
-    adset_name AS  adset_name,
-    account_id AS  account_id,
-    account_name AS  account_name,
-    campaign_id AS  campaign_id,
-    campaign_name AS  campaign_name,
-    device_platform AS  device_platform,
-    objective AS  objective,
-    publisher_platform AS  publisher_platform,
-    attribution_setting AS  attribution_setting,
-    TRIM(account_currency) AS  account_currency,
-    CAST(canvas_avg_view_percent AS FLOAT) AS  canvas_avg_view_percent,
-    CAST(canvas_avg_view_time AS FLOAT) AS  canvas_avg_view_time,
-    CAST(frequency AS FLOAT) AS  frequency,
-    CAST(clicks AS INT64) AS  clicks,
-    CAST(impressions AS INT64) AS impressions,
-    CAST(inline_post_engagement AS INT64) AS  inline_post_engagement,
-    CAST(inline_link_clicks AS INT64) AS inline_link_clicks,
-    CAST(reach AS INT64) AS reach,
-    CAST(spend as FLOAT) AS cost
+    source_relation,
+    _fivetran_id as fivetran_id,
+    date as day,
+    ad_id,
+    ad_name,
+    adset_id,
+    adset_name,
+    account_id,
+    account_name,
+    campaign_id,
+    campaign_name,
+    device_platform,
+    objective,
+    publisher_platform,
+    attribution_setting,
+    TRIM(account_currency) as account_currency,
+    CAST(canvas_avg_view_percent as FLOAT) as canvas_avg_view_percent,
+    CAST(canvas_avg_view_time as FLOAT) as canvas_avg_view_time,
+    CAST(frequency as FLOAT) as frequency,
+    CAST(clicks as INT64) as clicks,
+    CAST(impressions as INT64) as impressions,
+    CAST(inline_post_engagement as INT64) as inline_post_engagement,
+    CAST(inline_link_clicks as INT64) as inline_link_clicks,
+    CAST(reach as INT64) as reach,
+    CAST(spend as FLOAT) as cost
 
 from fields
--- models/staging/facebook_ads/stg_fivetran_facebooks_ads__ad_performance_v_1_action_values.sql
+++ models/staging/facebook_ads/stg_fivetran_facebooks_ads__ad_performance_v_1_action_values.sql
@@ -1,11 +1,12 @@
+with base as (
+    select *
+    from
+        {{ ref('stg_fivetran_facebook_ads__ad_performance_v_1_action_values_tmp') }}
+),
 
- with base as (
-   select * 
-   from {{ ref('stg_fivetran_facebook_ads__ad_performance_v_1_action_values_tmp') }}
-), 
 fields as (
-    select 
-    _fivetran_synced,
+    select
+        _fivetran_synced,
 
     {{
         fivetran_utils.fill_staging_columns(
@@ -18,21 +19,22 @@ fields as (
             fivetran_utils.source_relation()
             }}
 
-            from base
-        ),
+    from base
+),
+
 final as (
-   select 
-_fivetran_synced,
-      source_relation,
-      lower(action_type) as action_type,
-      index as idx,
-      _fivetran_id as fivetran_id,
-      ad_id,
-      date,
-      cast(coalesce(_1_d_view, 0) as {{dbt.type_float()}}) as _1_d_view,
-      cast(coalesce(_7_d_click, 0) as {{dbt.type_float()}}) as _7_d_click,
-      cast(coalesce(value, 0) as {{dbt.type_float()}}) as value
-      from fields
+    select
+        _fivetran_synced,
+        source_relation,
+        lower(action_type) as action_type,
+        index as idx,
+        _fivetran_id as fivetran_id,
+        ad_id,
+        date,
+        cast(coalesce(_1_d_view, 0) as {{ dbt.type_float() }}) as _1_d_view,
+        cast(coalesce(_7_d_click, 0) as {{ dbt.type_float() }}) as _7_d_click,
+        cast(coalesce(value, 0) as {{ dbt.type_float() }}) as value
+    from fields
 
 )
 
