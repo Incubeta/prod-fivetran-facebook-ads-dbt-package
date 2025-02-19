7b1487d8fcf1c60598c862ac366448b222630e6d
Miguelo Sana <miguelo@incubeta.com>
2025-02-19 16:24:15 +0100

    remove extra select

--- models/intermediate/facebook_ads/int_fivetran_facebook_ads_combined_actions.sql
+++ models/intermediate/facebook_ads/int_fivetran_facebook_ads_combined_actions.sql
@@ -1,6 +1,6 @@
 select
     {{ dbt_utils.star(from=ref('int_fivetran_facebook_ads__actions_types_metrics'), relation_alias='action_types_metrics', except=['source_relation', 'fivetran_id']) }},
-    {{ dbt_utils.star(from=ref('int_fivetran_facebook_ads_video_watch_actions_metrics'), relation_alias='video_watch_actions_metrics', except=['source_relation', 'fivetran_id', 'ad_id', 'date']) }}            {{dbt_utils.star(from=ref('int_fivetran_facebook_ads_video_watch_actions_metrics'), relation_alias='video_watch_actions_metrics', except=['source_relation', 'fivetran_id', 'ad_id', 'date'])}}
+    {{ dbt_utils.star(from=ref('int_fivetran_facebook_ads_video_watch_actions_metrics'), relation_alias='video_watch_actions_metrics', except=['source_relation', 'fivetran_id', 'ad_id', 'date']) }} 
 from
     {{ ref('int_fivetran_facebook_ads__actions_types_metrics') }}
         as action_types_metrics
